83fa0bb5e9736981ca4a9a64929c2275b8fc0ecd
Wrong1234 <mahabur24cse@gmail.com>
2026-06-10 08:52:05 +0600

    feat: admin dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './app/module/auth/auth.module';
 import { UserModule } from './app/module/user/user.module';
 import { PaymentModule } from './app/module/payment/payment.module';
 import { HistoryModule } from './app/module/history/history.module';
+import { DashboardModule } from './app/module/dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { HistoryModule } from './app/module/history/history.module';
     UserModule,
     PaymentModule,
     HistoryModule,
+    DashboardModule,
   ],
 
   controllers: [AppController],
